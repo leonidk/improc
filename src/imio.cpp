6353216d5b4f0d198b34b0da77a67f2e0f1b12fb
leonidk <lkeselman@gmail.com>
2015-04-19 17:20:40 -0700

    More IO is more fun

--- src/imio.cpp
+++ src/imio.cpp
@@ -18,7 +18,7 @@ namespace img {
 	Image<T,C> imread(const char * name) {
 		Image<T, C> returnImage = {};
 		int channels;
-		returnImage.data = std::shared_ptr<T>(stbi_load(name, &returnImage.width, &returnImage.height, &channels, C));
+		returnImage.data = std::shared_ptr<T>((T*)stbi_load(name, &returnImage.width, &returnImage.height, &channels, C));
 
 		return returnImage;
 	}
@@ -28,19 +28,24 @@ namespace img {
 		auto ext = getExtension(name);
 
 		if (ext == ".png") {
-			stbi_write_png(name, img.width, img.height, C, img.data.get(), 0);
+			stbi_write_png(name, img.width, img.height, C, (unsigned char*)img.data.get(), 0);
 		} else if (ext == ".tga") {						
-			stbi_write_tga(name, img.width, img.height, C, img.data.get());
+			stbi_write_tga(name, img.width, img.height, C, (unsigned char*)img.data.get());
 		} else if (ext == ".bmp") {						
-			stbi_write_bmp(name, img.width, img.height, C, img.data.get());
+			stbi_write_bmp(name, img.width, img.height, C, (unsigned char*)img.data.get());
 		} else if (ext == ".hdr") {						
 			stbi_write_hdr(name, img.width, img.height, C, (float*)img.data.get());
 		}
 	}
-	//template void imwrite(const char * name, const Image<uint8_t,1> &img);
+	template void imwrite(const char * name, const Image<uint8_t,1> &img);
+	template void imwrite(const char * name, const Image<uint8_t, 3> &img);
+	template void imwrite(const char * name, const Image<uint8_t, 4> &img);
+	template void imwrite(const char * name, const Image<float, 4> &img);
+
 	template Image<uint8_t, 1> imread(const char * name);
 	template Image<uint8_t, 3> imread(const char * name);
 	template Image<uint8_t, 4> imread(const char * name);
+	template Image<float, 4> imread(const char * name);
 
 
 }
