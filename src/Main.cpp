8cd4938346b37cd5cfc3df25553ac95b896dec6f
leonidk <lkeselman@gmail.com>
2015-05-30 14:27:18 -0700

    MNIST reading support

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,75 @@
 #include "image_io.h"
 #include "image_filter.h"
+
+#ifdef _WIN32 
+#include <intrin.h>
+#define bSwap _byteswap_ulong
+#else
+#define bSwap __builtin_bswap32
+#endif
+#include <vector>
+#include <fstream>
+std::vector<img::Img<uint8_t>> readMNISTImg(const std::string &fileName)
+{
+	std::vector<img::Img<uint8_t>> imgs;
+	std::ifstream file(fileName,std::ios::binary);
+	if (file.is_open())
+	{
+		uint32_t magic_number = 0;
+		uint32_t number_of_images = 0;
+		uint32_t n_rows = 0;
+		uint32_t n_cols = 0;
+		file.read((char*)&magic_number, sizeof(magic_number));
+		magic_number = bSwap(magic_number);
+		file.read((char*)&number_of_images, sizeof(number_of_images));
+		number_of_images = bSwap(number_of_images);
+		file.read((char*)&n_rows, sizeof(n_rows));
+		n_rows = bSwap(n_rows);
+		file.read((char*)&n_cols, sizeof(n_cols));
+		n_cols = bSwap(n_cols);
+		for (int i = 0; i<number_of_images; ++i)
+		{
+			img::Img<uint8_t> img(n_cols,n_rows);
+			file.read((char*)img.data.get(), n_rows*n_cols);
+			imgs.push_back(img);
+		}
+	}
+
+	return imgs;
+}
+
+std::vector<uint8_t> readMNISTLabel(const std::string &fileName)
+{
+	std::vector<uint8_t> labels;
+	std::ifstream file(fileName, std::ios::binary);
+	if (file.is_open())
+	{
+		uint32_t magic_number = 0;
+		uint32_t n_items = 0;
+		file.read((char*)&magic_number, sizeof(magic_number));
+		magic_number = bSwap(magic_number);
+		file.read((char*)&n_items, sizeof(n_items));
+		n_items = bSwap(n_items);
+		for (int i = 0; i<n_items; ++i)
+		{
+			uint8_t label;
+			file.read((char*)&label, 1);
+			labels.push_back(label);
+		}
+	}
+	return labels;
+}
+
+
 int main(int argc, char * argv[])
 {
+	auto train_img = readMNISTImg("train-images.idx3-ubyte");
+	auto train_lbl = readMNISTLabel("train-labels.idx1-ubyte");
+	int idx = 0;
 	do {
+		auto tl = train_lbl[idx];
+		auto t1 = train_img[idx++];
+		img::imshow("label",t1);
 		auto jpg = img::imread<uint8_t,3>("test.jpg");
 		auto png = img::imread<uint8_t, 3>("test.png");
 		auto jpg2 = img::boxFilter<11>(jpg);
