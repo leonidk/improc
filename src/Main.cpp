a43aead84d0f7e82bbf0448b81f54b989519b3ae
leonidk <lkeselman@gmail.com>
2016-03-28 15:19:07 -0700

    30x30x30 voxel grid classes

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,10 +9,11 @@
 #endif
 #include <vector>
 #include <fstream>
+#include <iostream>
 #include <random>
 #include <tuple>
 #include <algorithm>
-
+#include <cstring>
 std::vector<img::Img<uint8_t>> readMNISTImg(const std::string &fileName)
 {
 	std::vector<img::Img<uint8_t>> imgs;
@@ -59,6 +60,48 @@ std::vector<uint8_t> readMNISTLabel(const std::string &fileName)
 	}
 	return labels;
 }
+
+std::vector<uint8_t> readShapeNetLabel(const std::string &fileName)
+{
+    std::ifstream filelist(fileName);
+	std::vector<uint8_t> labels;
+	for (std::string line; std::getline(filelist, line); ) {
+        std::ifstream file(line, std::ios::binary);
+        if (file.is_open())
+        {
+            while (! file.eof() ) {
+                uint8_t label;
+                file.read((char*)&label, sizeof(uint8_t));
+                labels.push_back(label);
+            }
+        } else {
+            std::cout << "failed to open file\t" << line <<"\t"<< strerror(errno) << std::endl;
+        }
+    }
+	return labels;
+}
+struct grid30shape {
+    uint8_t data[30*30*30];
+};
+std::vector<grid30shape> readShapeNetData(const std::string &fileName)
+{
+    std::ifstream filelist(fileName);
+	std::vector<grid30shape> data;
+	for (std::string line; std::getline(filelist, line); ) {
+        std::ifstream file(line, std::ios::binary);
+        if (file.is_open())
+        {
+            while (! file.eof() ) {
+                grid30shape datum;
+                file.read((char*)&datum, sizeof(grid30shape));
+                data.push_back(datum);
+            }
+        } else {
+            std::cout << "failed to open file\t" << line <<"\t"<< strerror(errno) << std::endl;
+        }
+    }
+	return data;
+}
 #include <cereal/archives/binary.hpp>
 #include <cereal/archives/json.hpp>
 
@@ -76,36 +119,39 @@ public:
 		features(fernSize*numFerns)
 	{
 	}
-	void sampleFeatureFerns(int w, int h) 
+	void sampleFeatureFerns() 
 	{
-		std::uniform_int_distribution<int> wDist(0, w - 1);
-		std::uniform_int_distribution<int> hDist(0, h - 1);
+		std::uniform_int_distribution<int> wDist(0, 30 - 1);
+		std::uniform_int_distribution<int> hDist(0, 30 - 1);
+		std::uniform_int_distribution<int> dDist(0, 30 - 1);
 
 		for (int f = 0; f < numFerns; f++) {
 			for (int d = 0; d < fernSize; d++) {
-				features[f*fernSize + d] = std::make_tuple(wDist(gen), hDist(gen), wDist(gen), hDist(gen));
+				features[f*fernSize + d] = std::make_tuple(wDist(gen), hDist(gen), dDist(gen),wDist(gen), hDist(gen), dDist(gen));
 			}
 		}
 		probs = std::vector<float>(twoFernSize*(numClasses)*(numFerns), 1);
 		counts = std::vector<float>((numClasses), (1 << (fernSize)));
 	}
-	void sampleOneFern(int w, int h)
+	void sampleOneFern()
 	{
-		std::uniform_int_distribution<int> wDist(0, w - 1);
-		std::uniform_int_distribution<int> hDist(0, h - 1);
+		std::uniform_int_distribution<int> wDist(0, 30 - 1);
+		std::uniform_int_distribution<int> hDist(0, 30 - 1);
+		std::uniform_int_distribution<int> dDist(0, 30 - 1);
 		std::uniform_int_distribution<int> fDist(0, numFerns - 1);
 		auto f = fDist(gen);
 		for (int d = 0; d < fernSize; d++) {
-			features[f*fernSize + d] = std::make_tuple(wDist(gen), hDist(gen), wDist(gen), hDist(gen));
+			features[f*fernSize + d] = std::make_tuple(wDist(gen), hDist(gen), dDist(gen),wDist(gen), hDist(gen), dDist(gen));
 		}
 
 		probs = std::vector<float>(twoFernSize*(numClasses)*(numFerns), 1);
 		counts = std::vector<float>((numClasses), (1 << (fernSize)));
 	}
-	void sampleOneFeature(int w, int h)
+	void sampleOneFeature()
 	{
-		std::uniform_int_distribution<int> wDist(0, w - 1);
-		std::uniform_int_distribution<int> hDist(0, h - 1);
+		std::uniform_int_distribution<int> wDist(0, 30 - 1);
+		std::uniform_int_distribution<int> hDist(0, 30 - 1);
+		std::uniform_int_distribution<int> dDist(0, 30 - 1);
 		std::uniform_int_distribution<int> fDist(0, numFerns - 1);
 		std::uniform_int_distribution<int> ftDist(0, fernSize - 1);
 		std::bernoulli_distribution ft(0.5);
@@ -115,27 +161,30 @@ public:
 		auto which = ft(gen);
 		auto curr = features[f*fernSize + d];
 		features[f*fernSize + d] = which ? 
-            std::make_tuple((uint8_t)std::get<0>(curr), (uint8_t)std::get<1>(curr), (uint8_t)wDist(gen), (uint8_t)hDist(gen)) : 
-            std::make_tuple((uint8_t)wDist(gen), (uint8_t)hDist(gen), (uint8_t)std::get<2>(curr), (uint8_t)std::get<3>(curr));
+            std::make_tuple((uint8_t)std::get<0>(curr), (uint8_t)std::get<1>(curr),(uint8_t)std::get<2>(curr),
+                            (uint8_t)wDist(gen), (uint8_t)hDist(gen),(uint8_t)dDist(gen)) : 
+            std::make_tuple((uint8_t)wDist(gen), (uint8_t)hDist(gen), (uint8_t)dDist(gen),
+                            (uint8_t)std::get<3>(curr), (uint8_t)std::get<4>(curr),(uint8_t)std::get<5>(curr));
 
 		probs = std::vector<float>(twoFernSize*(numClasses)*(numFerns), 1);
 		counts = std::vector<float>((numClasses), (1 << (fernSize)));
 	}
-	int getHash(const img::Img<uint8_t> & img, const int fern) {
+	int getHash(const grid30shape & img, const int fern) {
 		int hash = 0;
-		auto ip = img.data.get();
 		for (int l = 0; l < fernSize; l++) {
 			auto feature = features[fern*fernSize + l];
 			auto p1x = std::get<0>(feature);
 			auto p1y = std::get<1>(feature);
-			auto p2x = std::get<2>(feature);
-			auto p2y = std::get<3>(feature);
-			int bit = (ip[img.width*p1y + p1x] > ip[img.width*p2y + p2x]) ? 1 : 0;
+			auto p1z = std::get<2>(feature);
+			auto p2x = std::get<3>(feature);
+			auto p2y = std::get<4>(feature);
+			auto p2z = std::get<5>(feature);
+			int bit = (img.data[(30*30)*p1z + 30*p1y + p1x] > img.data[(30*30)*p2z + 30*p2y + p2x]) ? 1 : 0;
 			hash |= (bit << l);
 		}
 		return hash;
 	}
-	void train(const img::Img<uint8_t> & img, const uint8_t label) {
+	void train(const grid30shape & img, const uint8_t label) {
 		for (int f = 0; f < numFerns; f++) {
 			auto hash = getHash(img, f);
 			probs[f*(numClasses*twoFernSize) + label*twoFernSize + hash]++;
@@ -144,7 +193,7 @@ public:
 
 	}
 	//void finishTraining() 
-	uint8_t predict(const img::Img<uint8_t> & img) {
+	uint8_t predict(const grid30shape & img) {
 		std::vector<float> fernClassProbs(numFerns*numClasses, 0); //tem
 		std::vector<float> fernSumProbs(numFerns, 0); //nrmzs
 
@@ -166,7 +215,7 @@ public:
 		for (int c = 0; c < numClasses; c++) {
 			auto prob = 1.0;
 			for (int f = 0; f < numFerns; f++) {
-				prob *= fernClassProbs[f*numClasses + c];
+				prob += logf(fernClassProbs[f*numClasses + c]);
 			}
 			classProbs[c] = prob;
 		}
@@ -188,35 +237,33 @@ public:
 private:
 	std::vector<float> probs; // 2^fernSize x numClasses x numFerns
 	std::vector<float> counts; // numClasses
-	std::vector<std::tuple<uint8_t, uint8_t, uint8_t, uint8_t>> features; // fernSize x numFerns. i(0,1) > i(2,3)
+	std::vector<std::tuple<uint8_t, uint8_t, uint8_t, uint8_t,uint8_t,uint8_t>> features; // fernSize x numFerns. i(0,1) > i(2,3)
 
 	int fernSize, numFerns, numClasses, twoFernSize;
 
 };
-
 int main(int argc, char * argv[])
 {
-	auto train_img = readMNISTImg("train-images.idx3-ubyte");
-	auto train_lbl = readMNISTLabel("train-labels.idx1-ubyte");
-	//auto test_img = readMNISTImg("train-images.idx3-ubyte");
-	//auto test_lbl = readMNISTLabel("train-labels.idx1-ubyte");
-	auto test_img = readMNISTImg("t10k-images.idx3-ubyte");
-	auto test_lbl = readMNISTLabel("t10k-labels.idx1-ubyte");
-	FernClassifier fc(13, 10, 10);
-	fc.sampleFeatureFerns(train_img[0].width, train_img[0].height);
-
-	{
+    auto train_lbl_sn = readShapeNetLabel("train_lbl.txt");
+    std::cout << train_lbl_sn.size() << std::endl;
+    auto train_raw_sn = readShapeNetData("train_raw.txt");
+    std::cout << train_raw_sn.size() << std::endl;
+    //for(int fernum = 19; fernum < 40; fernum++) {
+    //    for(int fernd = 8; fernd < 10; fernd++) {
+	FernClassifier fc(9, 29, 40);
+	fc.sampleFeatureFerns();
+	/*{
 		//83_out.json
 		std::ifstream is("97_out_7.json", std::ios::binary);
 		cereal::JSONInputArchive archive(is);
 		archive(fc);
-	}
+	}*/
 	FernClassifier bestFC = fc;
-	float bestAcc = 1.0;
+	float bestAcc =0.4;
 	float pAcc = bestAcc;
 	int iter = 0;
 	int iterations = 15000;
-	float startTmp = 1.0;
+	float startTmp = 0.1;
 	float minTmp = 0.00001;
 	float tmpFactor = 0.99;
 	float temp = startTmp;
@@ -224,21 +271,21 @@ int main(int argc, char * argv[])
 
 	while (true) {
 		fc = bestFC;
-		//fc.sampleFeatureFerns(train_img[0].width, train_img[0].height);
-		//fc.sampleOneFern(train_img[0].width, train_img[0].height);
-		//fc.sampleOneFeature(train_img[0].width, train_img[0].height);
-		for (int i = 0; i < train_img.size(); i++) {
-			fc.train(train_img[i], train_lbl[i]);
+        //fc.sampleFeatureFerns();
+		fc.sampleOneFern();
+		//fc.sampleOneFeature();
+		for (int i = 0; i < train_raw_sn.size(); i++) {
+			fc.train(train_raw_sn[i], train_lbl_sn[i]);
 		}
 		int predictQuality[10 * 10] = {};
 		float correct = 0;
-		for (int i = 0; i < test_img.size(); i++) {
-			auto res = fc.predict(test_img[i]);
-			predictQuality[10 * res + test_lbl[i]]++;
-			if (test_lbl[i] == res)
+        for (int i = 0; i < train_lbl_sn.size(); i+=1) {
+			auto res = fc.predict(train_raw_sn[i]);
+			predictQuality[10 * res + train_lbl_sn[i]]++;
+			if (train_lbl_sn[i] == res)
 				correct++;
 		}
-		float meanAccuracy = correct / test_img.size();
+		float meanAccuracy = correct / train_lbl_sn.size();
 		//std::cout << meanAccuracy << std::endl;
 		auto sqr = [](float x) { return x*x; };
 		auto CONST = 0.1f;
@@ -268,39 +315,19 @@ int main(int argc, char * argv[])
 			pAcc = meanAccuracy;
 			std::cout << "accepted" << std::endl;
 		}
-
+        //std::cout << fernum <<'\t' <<  fernd << std::endl;
 		std::cout << meanAccuracy <<"\t" << cost << "\t" << sample << "\t" << iter << "\t" << temp << std::endl;
 
 		iter++;
 		if (iter >= iterations) {
 			iter = 0;
-			fc.sampleFeatureFerns(train_img[0].width, train_img[0].height);
+			fc.sampleFeatureFerns();
 			bestFC = fc;
 			pAcc = 0.0;
 			temp = startTmp;
 		}
 	}
-	/*
-	int idx = 0;
-	do {
-		auto tl = train_lbl[idx];
-		auto t1 = train_img[idx++];
-		img::imshow("label",t1);
-		auto jpg = img::imread<uint8_t,3>("test.jpg");
-		auto png = img::imread<uint8_t, 3>("test.png");
-		auto jpg2 = img::boxFilter<11>(jpg);
-		img::imshow("jpg", jpg);
-		img::imshow("jpg2", jpg2);
-
-		auto gjpg = img::Rgb2grey(jpg);
-		jpg2 = img::grey2Rgb(gjpg);
-		auto gjpg2 = img::boxFilter<11>(gjpg);
-		img::imshow("jpg23", gjpg2);
-
-		auto intImg = img::intImage<uint8_t,1,uint32_t>(gjpg);
-		img::imshow("gjpg", gjpg);
-		img::imshow("png", png);
-	} while( 'q' != img::getKey(false));
-	*/
+  //      }
+  //  }
 	return 0;
 }
